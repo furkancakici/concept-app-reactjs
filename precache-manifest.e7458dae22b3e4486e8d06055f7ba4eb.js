08956c513351ca47b3cea12737b76a10cb414793
FurkanCakici <furkancakici25@gmail.com>
2020-10-09 00:43:15 +0300

    Updates

---
+++ precache-manifest.e7458dae22b3e4486e8d06055f7ba4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cb33814b89f37b88cb3c459904f863",
+    "url": "/concept-app-reactjs/index.html"
+  },
+  {
+    "revision": "98f2564fc98a2e644088",
+    "url": "/concept-app-reactjs/static/js/2.e1d658b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/concept-app-reactjs/static/js/2.e1d658b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02f8e72746d17b63e8e",
+    "url": "/concept-app-reactjs/static/js/main.857c1f31.chunk.js"
+  },
+  {
+    "revision": "a810be88c33f69eef46f",
+    "url": "/concept-app-reactjs/static/js/runtime-main.acefb041.js"
+  },
+  {
+    "revision": "4502ca5a04d5f3e43cdb8b6ca5d80d1f",
+    "url": "/concept-app-reactjs/static/media/svg-1.4502ca5a.svg"
+  },
+  {
+    "revision": "a3cb0dc791a8ea25889b5f5c118ccb30",
+    "url": "/concept-app-reactjs/static/media/svg-2.a3cb0dc7.svg"
+  }
+]);
